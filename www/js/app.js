b68aa491d0b4e3f05fbc8d74fab6180dc4ead12e
joeycchin <joeycchin@gmail.com>
2016-02-23 18:12:46 -0800

    merge conflicts

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,9 @@ angular.module('pug', ['ionic', 'pug.services', 'pug.auth', 'pug.map', 'pug.crea
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
