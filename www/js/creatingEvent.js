7b87c72d912333bc9f702d1a04765fb023d4f631
Jackie <77.Jackie@gmail.com>
2016-02-23 17:15:56 -0800

    seperate service and controller, service under Auth for now

--- www/js/creatingEvent.js
+++ www/js/creatingEvent.js
@@ -1,27 +1,9 @@
-angular.module('pug.creatingEvent', [])
-
-  .factory('creatingEventService', function ($http, $location, $window) {
-    var addEvent = function (createdEvent) {
-      return $http({
-        method: 'POST',
-        url: 'http://127.0.0.1:3000/api/events',
-        data: createdEvent
-      })
-      .then(function (data) {
-        //option 1) render a event successfully created page
-        //option 2) redirect to user's event listing page
-        return data;
-      })
-    }
-    return {
-      addEvent: addEvent
-    }
-  })
-  .controller('creatingEventCtrl', function (creatingEventService, $scope, $http, $location, $window) {
+angular.module('pug.creatingEvent', ['pug.services'])
+  .controller('creatingEventCtrl', function (Auth, $scope, $http, $location, $window) {
     $scope.createdEvent = {};
 
     $scope.addEvent = function () {
-      creatingEventService.addEvent($scope.createdEvent)
+      Auth.addEvent($scope.createdEvent)
       .then(function () {
         $location.path('/map');
       })
