ba9744b14d1e4dd99029b2e4955e350211ba7f97
Jackie <77.Jackie@gmail.com>
2016-02-24 16:39:03 -0800

    code styling fix

--- www/js/creatingEvent.js
+++ www/js/creatingEvent.js
@@ -1,80 +1,79 @@
 angular.module('creatingEvent', ['pug.services'])
-  .controller('creatingEventCtrl', function (Auth, $scope, $http, $location, $window, $state, $ionicHistory) {
-    $scope.createdEvent = {};
+.controller('creatingEventCtrl', function (Auth, $scope, $http, $location, $window, $state, $ionicHistory) {
+  $scope.createdEvent = {};
+  $scope.loc = [];
 
-    $scope.addEvent = function () {
-      Auth.addEvent($scope.createdEvent)
-      .then(function () {
-        Auth.set($scope.createdEvent);
-        $ionicHistory.clearCache()
-        .then(function (){ 
-          $state.go('map', {}, {reload: true})
-        }) 
-      })
-    }
+  $scope.addEvent = function () {
+    Auth.addEvent($scope.createdEvent)
+    .then(function () {
+      Auth.set($scope.createdEvent);
+      $ionicHistory.clearCache()
+      .then(function (){ 
+        $state.go('map', {}, {reload: true})
+      }) 
+    })
+  }
 
-    $scope.loc = [];
+  $scope.searchAddress = function () {
 
-    $scope.searchAddress = function () {
+    var addressInput = $scope.createdEvent.location;
+    var geocoder = new google.maps.Geocoder();
 
-      var addressInput = $scope.createdEvent.location;
-      var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({address: addressInput}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        $scope.createdEvent.latitude=results[0].geometry.location.lat();
+        $scope.createdEvent.longitude=results[0].geometry.location.lng();
+        $scope.addEvent();
 
-      geocoder.geocode({address: addressInput}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          $scope.createdEvent.latitude=results[0].geometry.location.lat();
-          $scope.createdEvent.longitude=results[0].geometry.location.lng();
-          $scope.addEvent();
-
-        }
-      });
-    }
-
-    function startTimePickerCallback(val) {
-      console.log(val);
-
-      if (typeof (val) === 'undefined') {
-        console.log('Time not selected');
-      } else {
-        var today = new Date();
-        var hour = parseInt(val/3600);
-        var minute = (val - hour*3600) / 60;
-        today.setUTCHours(hour);
-        today.setUTCMinutes(minute);
-        today.setUTCSeconds(0);
-
-        $scope.createdEvent.startTime = today;
       }
+    });
+  }
+
+  function startTimePickerCallback(val) {
+    console.log(val);
+
+    if (typeof (val) === 'undefined') {
+      console.log('Time not selected');
+    } else {
+      var today = new Date();
+      var hour = parseInt(val/3600);
+      var minute = (val - hour*3600) / 60;
+      today.setUTCHours(hour);
+      today.setUTCMinutes(minute);
+      today.setUTCSeconds(0);
+
+      $scope.createdEvent.startTime = today;
     }
+  }
 
-    function endTimePickerCallback(val) {
-      if (typeof (val) === 'undefined') {
-        console.log('Time not selected');
-      } else {
-        var today = new Date();
-        var hour = parseInt(val/3600);
-        var minute = (val - hour*3600) / 60;
-        today.setUTCHours(hour);
-        today.setUTCMinutes(minute);
-        today.setUTCSeconds(0);
-        $scope.createdEvent.endTime = today;
-      }
+  function endTimePickerCallback(val) {
+    if (typeof (val) === 'undefined') {
+      console.log('Time not selected');
+    } else {
+      var today = new Date();
+      var hour = parseInt(val/3600);
+      var minute = (val - hour*3600) / 60;
+      today.setUTCHours(hour);
+      today.setUTCMinutes(minute);
+      today.setUTCSeconds(0);
+      $scope.createdEvent.endTime = today;
     }
+  }
 
-    $scope.startTime = {
-      inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
-      format: 12,
-      callback: function (val) {
-        startTimePickerCallback(val);
-      }
-    };
+  $scope.startTime = {
+    inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
+    format: 12,
+    callback: function (val) {
+      startTimePickerCallback(val);
+    }
+  };
 
-    $scope.timePickerObject = {
-      inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
-      format: 12,
-      callback: function (val) {
-        endTimePickerCallback(val);
-      }
-    };
-  })
+  $scope.timePickerObject = {
+    inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
+    format: 12,
+    callback: function (val) {
+      endTimePickerCallback(val);
+    }
+  };
+})
 
